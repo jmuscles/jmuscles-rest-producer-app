e95435b734392ae31a274744b312a39d310ef29e
Manish Goel <manishkgoel@gmail.com>
2023-10-27 17:34:00 -0400

    working with and without db

--- src/main/java/com/jmuscles/rest/responsebuilder/CountryInfoServiceResponseBuilder.java
+++ src/main/java/com/jmuscles/rest/responsebuilder/CountryInfoServiceResponseBuilder.java
@@ -8,18 +8,23 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
-import com.jmuscles.rest.producer.config.RestConfPropsForConfigKey;
-import com.jmuscles.rest.producer.config.RestResponseConfig;
+import com.jmuscles.processing.schema.requestdata.RestRequestData;
+import com.jmuscles.rest.producer.config.properties.RestConfPropsForConfigKey;
+import com.jmuscles.rest.producer.config.properties.RestConfPropsForMethod;
+import com.jmuscles.rest.producer.config.properties.RestResponseConfig;
 import com.jmuscles.rest.producer.response.BaseResponseBuilder;
 import com.jmuscles.rest.producer.response.ResponseBuilderMapKeys;
 
 /**
  * This is the sample implementation for the response builder below key values
- * are available in the map to help build the response: {CONFIG_KEY,
- * HTTP_METHOD, HTTP_REQUEST, HTTP_HEADERS, REQUEST_BODY, IS_MESSAGE_QUEUED}
+ * are available in the map to help build the response: {RestRequestData,
+ * HTTP_REQUEST, IS_MESSAGE_QUEUED}
  * 
  */
 @Component
@@ -32,17 +37,46 @@ public class CountryInfoServiceResponseBuilder extends BaseResponseBuilder {
 
 	@Override
 	public ResponseEntity<?> buildResponse(Map<String, Object> map) {
+		logger.debug("Start building reponse...");
 
-		logger.debug("Start building response...");
-
-		String configKey = (String) map.get(ResponseBuilderMapKeys.CONFIG_KEY);
+		RestRequestData restRequestData = (RestRequestData) map.get(ResponseBuilderMapKeys.REST_REQUEST_DATA);
 		boolean queued = (boolean) map.get(ResponseBuilderMapKeys.IS_MESSAGE_QUEUED);
-		String method = (String) map.get(ResponseBuilderMapKeys.HTTP_METHOD);
 
-		RestResponseConfig response = restProducerConfigPropertiesMap.get(configKey).getConfigByHttpMethods()
-				.get(method).getResponseConfig().get(queued ? "success" : "failure");
-		return new ResponseEntity<>(response.getBody(), response.getHeaders(), response.getStatus());
+		Map<String, RestConfPropsForMethod> restConfPropsForMethodmap = restProducerConfigPropertiesMap
+				.get(restRequestData.getConfigKey()).getConfigByHttpMethods();
 
+		ResponseEntity<?> responseEntity = null;
+
+		if (restConfPropsForMethodmap != null && restConfPropsForMethodmap.get(restRequestData.getMethod()) != null
+				&& restConfPropsForMethodmap.get(restRequestData.getMethod()).getResponseConfig() != null) {
+			RestResponseConfig response = restConfPropsForMethodmap.get(restRequestData.getMethod()).getResponseConfig()
+					.get(queued ? "success" : "failure");
+			if (response != null) {
+				responseEntity = new ResponseEntity<>(response.getBody(), convertToMultiValueMap(response.getHeaders()),
+						response.getStatus());
+			}
+		}
+
+		if (responseEntity == null) {
+			logger.error("There is no configuartion to build reponse for configKey:{}, method:{} and {} scenario",
+					restRequestData.getConfigKey(), restRequestData.getMethod(), (queued ? "success" : "failure"));
+			if (queued) {
+				responseEntity = ResponseEntity.ok("Request is queued to be processed");
+			} else {
+				responseEntity = new ResponseEntity<>("Request could not be queued", HttpStatus.INTERNAL_SERVER_ERROR);
+			}
+		}
+		logger.debug("... reponse built successfully");
+		return responseEntity;
+
+	}
+
+	public <K, V> MultiValueMap<K, V> convertToMultiValueMap(Map<K, V> map) {
+		MultiValueMap<K, V> multiValueMap = new LinkedMultiValueMap<>();
+		for (Map.Entry<K, V> entry : map.entrySet()) {
+			multiValueMap.add(entry.getKey(), entry.getValue());
+		}
+		return multiValueMap;
 	}
 
 }
